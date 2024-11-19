72bdb54afa5530aa5c46ec98297bb4c1bbfa3fa4
Osmely Fernandez <osmely@kinesis.co.il>
2024-11-18 22:27:43 -0500

    WIP

--- android/src/main/java/com/kinesis/flutter_adm/FlutterAdmPlugin.java
+++ android/src/main/java/com/kinesis/flutter_adm/FlutterAdmPlugin.java
@@ -50,8 +50,8 @@ public class FlutterAdmPlugin implements FlutterPlugin, MethodCallHandler {
       final String id = adm.getRegistrationId();
       if (id == null){
 
-        var callArgs: HashMap<String, Any> = call.arguments as HashMap<String, Any>;
-        String topic = callArgs["topic"] as String;
+        HashMap<String, Object> argsMap = (HashMap<String, Object>) call.arguments;
+        String topic = argsMap.get("topic");
 
         Log.d("FlutterAdmPlugin", "Received topic: " + topic);
           
