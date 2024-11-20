d2c1492a34c36c957b2a38619bfc9d4424b64b33
Osmely Fernandez <osmely@kinesis.co.il>
2024-11-19 20:46:54 -0500

    WIP

--- android/src/main/java/com/kinesis/flutter_adm/FlutterAdmPlugin.java
+++ android/src/main/java/com/kinesis/flutter_adm/FlutterAdmPlugin.java
@@ -4,7 +4,8 @@ import androidx.annotation.NonNull;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
-
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
 
 /** FlutterAdmPlugin */
 public class FlutterAdmPlugin implements FlutterPlugin, MethodCallHandler {
