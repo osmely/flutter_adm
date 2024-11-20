f84487ffbfd69718bc54da24c4f0d1762e07002f
Osmely Fernandez <osmely@kinesis.co.il>
2024-11-20 11:15:41 -0500

    WIP

---
+++ android/src/main/java/com/kinesis/flutter_adm/ADMConstants.java
@@ -0,0 +1,19 @@
+
+package com.amazon.sample.admmessenger;
+
+public class ADMConstants {
+
+    // JSON keys
+    public static final String JSON_DATA_MSG_KEY = "message";
+    public static final String JSON_DATA_TIME_KEY = "timeStamp";
+    public static final String JSON_DATA_CONSOLIDATION_KEY = "collapse_key";
+
+    // Intent keys
+    public static final String INTENT_MSG_ACTION = "com.amazon.sample.admmessenger.ON_MESSAGE";
+    public static final String INTENT_MSG_CATEGORY = "com.amazon.sample.admmessenger.MSG_CATEGORY";
+
+    // Notification ID
+    public static final int SAMPLE_APP_NOTIFICATION_ID = 12345678;
+
+    private ADMConstants() {}
+}
