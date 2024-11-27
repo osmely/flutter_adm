1107d8ea104d966cdc9f3708e8deab35b3b728c0
Osmely Fernandez <osmely@kinesis.co.il>
2024-11-27 15:30:42 -0500

    feat: Convert Intent extras to JSON string in onMessage method

--- android/src/main/java/com/kinesis/flutter_adm/PluginADMMessageHandlerJobBase.java
+++ android/src/main/java/com/kinesis/flutter_adm/PluginADMMessageHandlerJobBase.java
@@ -19,6 +19,10 @@ import com.amazon.device.messaging.ADMMessageHandlerJobBase;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import org.json.JSONObject;
+import org.json.JSONException;
+import android.util.Log;
+
 
 /**
  * The PluginADMMessageHandlerJobBase class receives messages sent by ADM via the SampleADMMessageReceiver receiver.
@@ -44,7 +48,25 @@ public class PluginADMMessageHandlerJobBase extends ADMMessageHandlerJobBase
     @Override
     protected void onMessage(final Context context, final Intent intent)
     {
-        Log.i(TAG, "PluginADMMessageHandlerJobBase:onMessage");
+        
+
+        Bundle extras = intent.getExtras();
+        JSONObject jsonExtras = new JSONObject();
+        
+        if (extras != null) {
+            for (String key : extras.keySet()) {
+                try {
+                    jsonExtras.put(key, extras.get(key));
+                } catch (JSONException e) {
+                    Log.e(TAG, "Error converting extras to JSON", e);
+                }
+            }
+        }
+        
+        String messageData = jsonExtras.toString();
+        mainHandler.post(() -> sendMessageToDart(messageData));
+
+
 
         /* String to access message field from data JSON. */
         final String msgKey = PluginADMConstants.JSON_DATA_MSG_KEY;
@@ -55,10 +77,6 @@ public class PluginADMMessageHandlerJobBase extends ADMMessageHandlerJobBase
         /* Intent action that will be triggered in onMessage() callback. */
         final String intentAction = PluginADMConstants.INTENT_MSG_ACTION;
 
-        /* Extras that were included in the intent. */
-        final Bundle extras = intent.getExtras();
-
-        verifyMD5Checksum(context, extras);
 
         /* Extract message from the extras in the intent. */
         final String msg = extras.getString(msgKey);
@@ -70,9 +88,6 @@ public class PluginADMMessageHandlerJobBase extends ADMMessageHandlerJobBase
                     "Make sure that msgKey and timeKey values match data elements of your JSON message");
         }
 
-        /* Create a notification with message data. */
-        /* This is required to test cases where the app or device may be off. */
-        ADMHelper.createADMNotification(context, msgKey, timeKey, intentAction, msg, time);
 
         /* Intent category that will be triggered in onMessage() callback. */
         final String msgCategory = PluginADMConstants.INTENT_MSG_CATEGORY;
@@ -89,40 +104,6 @@ public class PluginADMMessageHandlerJobBase extends ADMMessageHandlerJobBase
 
     }
 
-    /**
-     * This method verifies the MD5 checksum of the ADM message.
-     *
-     * @param extras Extra that was included with the intent.
-     */
-    private void verifyMD5Checksum(final Context context, final Bundle extras)
-    {
-        /* String to access consolidation key field from data JSON. */
-        final String consolidationKey = PluginADMConstants.JSON_DATA_CONSOLIDATION_KEY;
-
-        final Set<String> extrasKeySet = extras.keySet();
-        final Map<String, String> extrasHashMap = new HashMap<String, String>();
-        for (String key : extrasKeySet)
-        {
-            if (!key.equals(ADMConstants.EXTRA_MD5) && !key.equals(consolidationKey))
-            {
-                extrasHashMap.put(key, extras.getString(key));
-            }
-        }
-        final String md5 = ADMMD5ChecksumCalculator.calculateChecksum(extrasHashMap);
-        Log.i(TAG, "PluginADMMessageHandlerJobBase:onMessage App md5: " + md5);
-
-        /* Extract md5 from the extras in the intent. */
-        final String admMd5 = extras.getString(ADMConstants.EXTRA_MD5);
-        Log.i(TAG, "PluginADMMessageHandlerJobBase:onMessage ADM md5: " + admMd5);
-
-        /* Data integrity check. */
-        if(!admMd5.trim().equals(md5.trim()))
-        {
-            Log.w(TAG, "PluginADMMessageHandlerJobBase:onMessage MD5 checksum verification failure. " +
-                    "Message received with errors");
-        }
-    }
-
     /** {@inheritDoc} */
     @Override
     protected void onRegistrationError(final Context context, final String string)
@@ -130,16 +111,11 @@ public class PluginADMMessageHandlerJobBase extends ADMMessageHandlerJobBase
         
     }
 
-    public static void sendRegistrationIdToDart(String registrationId) {
-        FlutterAdmPlugin.sendRegistrationIdToDart(registrationId);
-    }
-
     /** {@inheritDoc} */
     @Override
     protected void onRegistered(final Context context, final String registrationId)
     {
         mainHandler.post(() -> sendRegistrationIdToDart(registrationId));
-        // FlutterAdmPlugin.sendRegistrationIdToDart(registrationId);
     }
 
     /** {@inheritDoc} */
@@ -169,4 +145,12 @@ public class PluginADMMessageHandlerJobBase extends ADMMessageHandlerJobBase
         
     }
 
+
+    public static void sendRegistrationIdToDart(String registrationId) {
+        FlutterAdmPlugin.sendRegistrationIdToDart(registrationId);
+    }
+
+    public static void sendMessageToDart(String message) {
+        FlutterAdmPlugin.sendMessageToDart(message);
+    }
 }
