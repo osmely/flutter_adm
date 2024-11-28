2194c688072f35d38d63eff99a6cba451d49e6dd
Osmely Fernandez <osmely@kinesis.co.il>
2024-11-28 17:45:38 -0500

    WIP

--- android/src/main/java/com/kinesis/flutter_adm/PluginADMMessageHandlerJobBase.java
+++ android/src/main/java/com/kinesis/flutter_adm/PluginADMMessageHandlerJobBase.java
@@ -76,13 +76,14 @@ public class PluginADMMessageHandlerJobBase extends ADMMessageHandlerJobBase
                 mainHandler.post(() -> sendMessageToDart(messageData));
                 
             } else {
+
                 // If app is in background, show notification
-                // showNotification(context, jsonExtras);
+                showNotification(context, jsonExtras);
+
+                
             }
         }
 
-         // Prevenir la notificación por defecto de ADM
-        excludeMessageFromAppNotifications(intent);
         
         // String messageData = jsonExtras.toString();
         // mainHandler.post(() -> sendMessageToDart(messageData));
